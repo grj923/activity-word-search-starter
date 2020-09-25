41c3c9e7889f7356e1bdb91c445e2b0f3b493e4d
Greg Johnson <grj923@gmail.com>
2020-09-25 17:58:21 -0400

    word search counter trying it

--- assets/main.js
+++ assets/main.js
@@ -1,69 +1,18 @@
-// COMPLETE THE TODOs BELOW...
-// Note: The primary TODOs for this Activity will not require
-// you to edit the HTML or CSS.
-
-// On our HTML page, we have a #search-button, a #textbox input,
-// box a #sentence, and a #search-results DIV.
-
-// TODO 1: Use querySelector to assign EACH of the above
-// elements to a variable, just like this:
 let searchButton = document.querySelector("#search-button");
 let textBox = document.querySelector("#textbox");
 let sentence = document.querySelector("#sentence");
 let searchResult = document.querySelector("#search-results");
+let words = sentence.innerText.split(/\s/);
+let counter = 0;
 
 searchButton.addEventListener("click", function () {
   console.log(textBox.value);
   console.log(sentence.innerText);
-
-  // let input = (sentence.innerText);
-  // let arrayOfWords = input.split(" ");
-  // let wordCounts = {};
-  // for (i = 0; i < arrayOfWords.length; i++) {
-  // let textBox.value = arrayOfWords[i];
-  // if (!wordCounts[textBox.value]){
-  // wordCounts[textBox.value] = 1;
-  // } else {
-  // wordCounts[textBox.value]++;
-  // }
-  //
-  // }
-
-  // if (textBox.value.includes(sentence.innerText)) {
-  if (sentence.innerText.toLowerCase().includes(textBox.value.toLowerCase())) {
-    console.log("Found it");
-    searchResult.innerText = "Match! Good Job!";
-  } else {
-    searchResult.innerText = "Too Bad!";
-  }
 });
-// TODO 2: Write an IF statement which determines whether
-// the user-supplied string from #textbox is included in
-// the #sentence string.
-// Hint 1: To get the user-supplied string from the input
-// box (#textbox), use the property .value on the variable
-// you assigned the textbox element to.
-// Hint 2: To get the string contained within #sentence, use
-// the property .innerText on the variable you assigned the
-// #sentence element to.
-// Hint 3: You can check whether string1 contains string2
-// by using string1.includes(string2) – which will return
-// true or false.
-// TODO 3: If the user-supplied string is included in the
-// #sentence string, update the innerText of #search-results
-// with a success message (such as, "A match was found!"),
-// otherwise update it with a failure message (such as,
-// "No results. Too bad!")
-
-// STRETCH GOALS (easiest to hardest):
-//
-//  • Customize the CSS to your liking.
-//  • Use .trim() to remove unnecessary whitespace from the
-//    user-supplied string.
-//  • Use .toLowerCase() on both the sentence and the user-
-//    supplied string, so that your word search is
-//    NOT case-sensitive.
-//  • Count the number of times the word appears.
-//  • Display the word count for the user.
-//  • Find a way to highlight the matching word(s).
-//    Hint: .split() and Iteration.
+for (let i = 0; i < words.length; i++) {
+  if (words[i].includes("of")) {
+    counter++;
+    console.log(counter);
+    searchResult.innerText = "Match!";
+  }
+}
